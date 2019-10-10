88e9311eabe941ed79ce235464099c36c038aaeb
lee-jong <44047449+lee-jong@users.noreply.github.com>
2019-10-10 17:48:10 +0900

    timerRandom user usage time

---
+++ pages/developmentNote/count/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+class Count extends React.Component {
+  state = {};
+
+  users = ['Kevin', 'Alax', 'Brod', 'Jong'];
+
+  onStartTimer = e => {
+    e.target.disabled = 'disabled';
+    let id = e.target.name;
+    console.log('start', id);
+
+    this[e.target.name] = setInterval(() => {
+      if (this.state[id]) {
+        this.setState({
+          [id]: ++this.state[id]
+        });
+      } else {
+        let defaultTime = 0;
+        this.setState({
+          [id]: ++defaultTime
+        });
+      }
+    }, 1000);
+  };
+
+  onEndTimter = e => {
+    e.target.previousSibling.disabled = '';
+    console.log('stop', e.target.name);
+    clearInterval(this[e.target.name]);
+  };
+
+  changeTimer = time => {
+    let getTime;
+    if (time <= 60) {
+      return (getTime = `${time}초`);
+    }
+    if (time > 60 && time < 3600) {
+      let mm = parseInt(time / 60);
+      let ss = time % 60;
+      getTime = `${mm}분 ${ss}초`;
+      return getTime;
+    }
+
+    if (time > 3600) {
+      let hh = parseInt(time / 3600);
+      let mm = parseInt((time % 3600) / 60);
+      let ss = time % 60;
+      getTime = `${hh}시 ${mm}분 ${ss}초`;
+      return getTime;
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          {this.users.map(user => (
+            <div>
+              <h3>
+                {user} :
+                {this.state[user] ? this.changeTimer(this.state[user]) : '0초'}
+              </h3>
+              <div>
+                <button name={user} onClick={this.onStartTimer}>
+                  시작
+                </button>
+                <button name={user} onClick={this.onEndTimter}>
+                  멈춤
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Count;
